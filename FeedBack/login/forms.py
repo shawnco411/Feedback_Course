85cd0b0a75c2487e51daf2e31c42088c9634d2fa
mxj <1262539334@qq.com>
2019-03-29 19:53:53 +0800

    mxj commit

---
+++ FeedBack/login/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+class RegisterForm(forms.Form):
+    gender = (
+        ('student', '学生'),
+        ('teacher', '教师'),
+        ('assistant', '助教'),
+    )
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    identity = forms.ChoiceField(label='身份', choices=gender)
+    #captcha = CaptchaField(label='验证码')
