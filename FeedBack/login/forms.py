098515038c73de04ddd5762eca13ee519cdb93fa
zhc16061015 <1294552164@qq.com>
2019-04-17 16:25:56 +0800

    nothing

--- FeedBack/login/forms.py
+++ FeedBack/login/forms.py
@@ -3,8 +3,8 @@ from login.models import Homework,SubmitWork
 
 
 class UserForm(forms.Form):
-    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'userID'}))
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': 'password'}))
 
 class RegisterForm(forms.Form):
     idens = (
@@ -12,10 +12,10 @@ class RegisterForm(forms.Form):
         ('teacher', '教师'),
         ('assistant', '助教'),
     )
-    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': '例如: user123'}))
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': '建议6-16位包含字母数字'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': '请和上一次输入相同'}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control','placeholder': '例如: 123@qq.com'}))
     identity = forms.ChoiceField(label='身份', choices=idens)
     #captcha = CaptchaField(label='验证码')
 
@@ -71,7 +71,7 @@ class SubmitForm(forms.ModelForm):
     myfile = forms.FileField(
         label="上传文件",
         widget=forms.FileInput(
-            attrs={'class': 'form-control', 'placeholder': '上传文件'}
+            #attrs={'class': 'form-control', 'placeholder': '上传文件'}
         )
     )
     class Meta:
